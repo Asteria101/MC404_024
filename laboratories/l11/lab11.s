942f1bc44df405ac484df05dfc81574d13897ea3
Asteria101 <lebrito1268@gmail.com>
2024-10-21 02:02:37 -0300

    Add improved version of lab 11

--- laboratories/l11/lab11.s
+++ laboratories/l11/lab11.s
@@ -6,7 +6,49 @@
     We need to get it to position (73, 1, -19)
 */
 
+keep_going:
+    # a0 has the limit for the loop
+    li t0, 0
+    0:
+        # get the car's coordinates
+        li t1, 0xFFFF0110
+        lw t2, (t1) # x coordinate
+        li t1, 0xFFFF0114
+        lw t3, (t1) # y coordinate
+        li t1, 0xFFFF0118
+        lw t4, (t1) # z coordinate
+
+        beq t0, a0, 0f
+        addi t0, t0, 1
+        j 0b
+    0:
+    ret
+
+
+break:
+    # loop for stopping the car
+    # a0 has the limit for the loop
+
+    li t0, 0
+    0:
+        # activates the brake
+        li a1, 0xFFFF0122
+        li t1, 1
+        sb t1, (a1)
+
+        beq t0, a0, 0f
+        addi t0, t0, 1
+        j 0b
+    0:
+    # sets car's engine to go off
+    li a0, 0xFFFF0121
+    li t1, 0
+    sb t1, (a0)
+    ret
+
+
 _start:
+    # initial setup
     li a0, 0xFFFF0100 # address of the first byte of the memory-mapped I/O region
     li a1, '1' # store into a1
     sb a1, (a0) # store a1 into a0, to start reading the coordinates and rotation
@@ -20,69 +62,31 @@ _start:
     # sets car's steering wheel to go only forward
     li a0, 0xFFFF0120
     li t0, 0
-    sb t0, (a0) # steering wheel direction set to forward 
+    sb t0, (a0)
+    li a0, 10000
+    jal keep_going
 
-    li t0, 10000
-    li t1, 0
-    0:
-        li a0, 0xFFFF0110
-        lw a1, (a0) # x coordinate
-        li a0, 0xFFFF0114
-        lw a2, (a0) # y coordinate
-        li a0, 0xFFFF0118
-        lw a3, (a0) # z coordinate
-        beq t1, t0, 0f
-        addi t1, t1, 1
-        j 0b
-    0:
-
-    # Start turnning the car
-    li a0, 0xFFFF0121
-    li t0, 1
-    sb t0, (a0) # engine direction set to forward
-
+    # Start turnning the car 91 degrees to the left
     li a0, 0xFFFF0120
     li t0, -90
-    sb t0, (a0) # steering wheel direction set to 45 degress 
+    sb t0, (a0)
+    li a0, 6500
+    jal keep_going
 
-    li t0, 6500
-    li t1, 0
-    1:
-        li a0, 0xFFFF0110
-        lw a1, (a0) # x coordinate
-        li a0, 0xFFFF0114
-        lw a2, (a0) # y coordinate
-        li a0, 0xFFFF0118
-        lw a3, (a0) # z coordinate
-        beq t1, t0, 1f
-        addi t1, t1, 1
-        j 1b
-    1:
-
-    # go forward
-    li a0, 0xFFFF0121
-    li t0, 1
-    sb t0, (a0) # engine direction set to forward
-
+    # after turning the car, we need to go a bit forward
     li a0, 0xFFFF0120
     li t0, 0
     sb t0, (a0) # steering wheel direction set to 0
+    li a0, 3000
+    jal keep_going
 
     # loop for stopping the car
-    li t0, 3400
-    li t1, 0
-    2:
-        li a0, 0xFFFF0122
-        li t2, 1
-        sb t2, (a0)
-        beq t1, t0, 2f
-        addi t1, t1, 1
-        j 2b
-    2:
+    li a0, 7000
+    jal break
 
-    li a0, 0xFFFF0100 # address of the first byte of the memory-mapped I/O region
-    li a1, '0' # store into a1
-    sb a1, (a0) # store a1 into a0, to start reading the coordinates and rotation
+    li a0, 0xFFFF0100 
+    li a1, '0'
+    sb a1, (a0) # store '0' into a0, to stop reading the coordinates and rotation
 
     li a0, 0 # return value
     li a7, 93 # exit syscall
